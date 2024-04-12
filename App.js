702215ff17f49aaed27756bc6e5ab17445e8464c
ManvithaBattula <bmanvitha@gmail.com>
2024-04-12 23:23:28 +0530

    React injection using CDM links

--- App.js
+++ App.js
@@ -1,3 +1,15 @@
-const header = React.createElement("div",{},"Hello World from React")
+const header = React.createElement("div",{},
+[
+    React.createElement("div",{id:"child1"},
+    [
+        React.createElement("h1",{},"Hello World from React"),
+        React.createElement('h1',{},"I am a H1 tag")
+    ]),
+    React.createElement("div",{id:"child2"},
+    [
+        React.createElement("h1",{},"Hello I am a H1 tag"),
+        React.createElement("h2",{},"I am a H2 tag")
+    ])
+])
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(header)
