d174285e8211d6dfabcc04bf8db2abd96fb8f835
ManvithaBattula <bmanvitha@gmail.com>
2024-04-13 23:35:11 +0530

    React injection using node modules and parcel installation and hosting the application onto the local server

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 const header = React.createElement("div",{},
 [
     React.createElement("div",{id:"child1"},
     [
         React.createElement("h1",{},"Hello World from React"),
-        React.createElement('h1',{},"I am a H1 tag")
+        React.createElement('h1',{},"I am a Manvitha")
     ]),
     React.createElement("div",{id:"child2"},
     [
